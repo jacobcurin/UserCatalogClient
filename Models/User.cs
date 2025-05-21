6b9d9b76c311d0e2373b52b3dd0ad319259d7a36
Jacob Curin Antinao <jacob.curin.9@gmail.com>
2025-05-21 19:19:13 -0400

    Se modifican propiedades para que acepten valores nulos

--- Models/User.cs
+++ Models/User.cs
@@ -5,25 +5,25 @@ namespace UserCatalogMvc.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
 
-        public Address Address { get; set; }
-        public string Phone { get; set; }
-        public string Website { get; set; }
-        public Company Company { get; set; }
+        public Address? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Website { get; set; }
+        public Company? Company { get; set; }
     }
 
     public class Address
     {
-        public string Street { get; set; }
-        public string Suite {  get; set; }
-        public string City { get; set; }
+        public string? Street { get; set; }
+        public string? Suite {  get; set; }
+        public string? City { get; set; }
     }
 
     public class Company
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
     }
 }
