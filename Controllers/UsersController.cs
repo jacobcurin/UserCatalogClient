1047d549eca8c879d8e1900491ea4f7cdeca5910
Jacob Curin Antinao <jacob.curin.9@gmail.com>
2025-05-23 19:45:22 -0400

    Se implementa funcion para descargar tabla de datos en formato CSV

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using UserCatalogMvc.Models;
 using System.Collections.Generic;
+using System.Text;
 
 namespace UserCatalogMvc.Controllers
 {
@@ -21,7 +22,7 @@ namespace UserCatalogMvc.Controllers
 
             if (!response.IsSuccessStatusCode)
             {
-                return View("Error mi insano, revisalo");
+                return View("Error al intentar visualizar los datos");
             }
 
             var json = await response.Content.ReadAsStringAsync();
@@ -32,5 +33,35 @@ namespace UserCatalogMvc.Controllers
 
             return View(users);
         }
+
+        //Metodo para exportar la lista de datos
+        [HttpGet]
+        public async Task<IActionResult> ExportCsv()
+        {
+            var response = await _httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return Content("Error al obtener los datos.");
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var users = JsonSerializer.Deserialize<List<User>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            var csv = new StringBuilder();
+            csv.AppendLine("Nombre;Usuario;Email;Ciudad;Empresa");
+
+            foreach (var user in users)
+            {
+                csv.AppendLine($"{user.Name};{user.Username};{user.Email};{user.Address.City};{user.Company.Name}");
+            }
+
+            var bytes = System.Text.Encoding.UTF8.GetBytes(csv.ToString());
+            return File(bytes, "text/csv", "usuarios.csv");
+        }
+
     }
 }
